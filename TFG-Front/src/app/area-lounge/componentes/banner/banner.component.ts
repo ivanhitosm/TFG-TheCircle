d82fff5a7f58f8b8f6c96af5d10239b95adc6896
JaviLRZ <javierlopezramirez.97@gmail.com>
2022-03-15 17:30:47 +0100

    añadidas modificaciones

--- TFG-Front/src/app/area-lounge/componentes/banner/banner.component.ts
+++ TFG-Front/src/app/area-lounge/componentes/banner/banner.component.ts
@@ -1,17 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['../../../../assets/css/styles.css'],
-
 })
-export class BannerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BannerComponent {
+ 
 }
-
