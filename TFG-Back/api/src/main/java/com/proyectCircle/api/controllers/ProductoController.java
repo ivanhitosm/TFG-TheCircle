076f527c358ccfcaa17e437c7f2bd712e41f4b83
ivanhitosm <ivanhitosm@gmail.com>
2022-04-13 21:15:26 +0200

    updateando CORS

--- TFG-Back/api/src/main/java/com/proyectCircle/api/controllers/ProductoController.java
+++ TFG-Back/api/src/main/java/com/proyectCircle/api/controllers/ProductoController.java
@@ -17,42 +17,42 @@ public class ProductoController {
   @Autowired
   private ProductoService service;
   
-
+  @CrossOrigin(origins = "http://localhost:4200")
   @PostMapping("/addProducto")
   public ProductoModel addProducto(@RequestBody ProductoModel producto) {
     return service.guardarProducto(producto);
   }
-
+  @CrossOrigin(origins = "http://localhost:4200")
   @PostMapping("/addListaProductos")
   public List<ProductoModel> addProductos(@RequestBody List<ProductoModel> producto) {
     return service.guardarProductos(producto);
   }
-
+  @CrossOrigin(origins = "http://localhost:4200")
   @GetMapping("/Productos")
   public List<ProductoModel> todosProductos() {
     return service.getProductos();
   }
-
+  @CrossOrigin(origins = "http://localhost:4200")
   @GetMapping("/ProductoId/{id}")
   public ProductoModel productosId(@PathVariable long id) {
     return service.getProductoById(id);
   }
-
+  @CrossOrigin(origins = "http://localhost:4200")
   @GetMapping("/ProductoNombre/{nombre}")
   public List<ProductoModel> productosId(@PathVariable String nombre) {
     return service.getProductosByNombre(nombre);
   }
-
+  @CrossOrigin(origins = "http://localhost:4200")
   @PutMapping("/updateProducto")
   public ProductoModel updateProducto(@RequestBody ProductoModel producto) {
     return service.actualizarProducto(producto);
   }
-
+  @CrossOrigin(origins = "http://localhost:4200")
   @DeleteMapping("/delete/{id}")
   public String deleteProducto(@PathVariable long id) {
     return service.deleteProducto(id);
   }
-
+  @CrossOrigin(origins = "http://localhost:4200")
   @GetMapping("/Productos/pagina/")
     public Page<ProductoModel> findProductsWithPaginationAndSortingVisible(
             @RequestParam(value="offset", defaultValue = "0") Integer offset,
@@ -60,6 +60,7 @@ public class ProductoController {
             @RequestParam(value="field",defaultValue = "id") String field)  {
      return service.findProductsWithPaginationAndSortingVisible(offset,pageSize,field);
   }
+  @CrossOrigin(origins = "http://localhost:4200")
   @GetMapping("/Productos/paginaAll/")
     public Page<ProductoModel> findProductsWithPaginationAndSorting(
             @RequestParam(value="offset", defaultValue = "0") Integer offset,
