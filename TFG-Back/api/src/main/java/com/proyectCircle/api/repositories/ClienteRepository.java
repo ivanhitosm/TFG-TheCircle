8c0fd4cacf3d3dc3f0336d85464b50842cf6fe58
ivanhitosm <73246987+ivanhitosm@users.noreply.github.com>
2022-05-20 10:26:28 +0200

    Create ClienteRepository.java

---
+++ TFG-Back/api/src/main/java/com/proyectCircle/api/repositories/ClienteRepository.java
@@ -0,0 +1,23 @@
+package com.proyectCircle.api.repositories;
+
+
+import java.util.List;
+
+
+import com.proyectCircle.api.models.ClienteModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
+
+    ClienteModel findByNombre(String nombre);
+
+    List<ClienteModel> findAllByNombreContaining(String nombre);
+
+   
+
+
+    
+}
