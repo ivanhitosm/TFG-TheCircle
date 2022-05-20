29437eee6b94ce2c663afcf1123b5ca9e015110f
ivanhitosm <73246987+ivanhitosm@users.noreply.github.com>
2022-05-20 10:29:45 +0200

    Create VariacionRepository.java

---
+++ TFG-Back/api/src/main/java/com/proyectCircle/api/repositories/VariacionRepository.java
@@ -0,0 +1,23 @@
+package com.proyectCircle.api.repositories;
+
+
+import java.util.List;
+
+
+import com.proyectCircle.api.models.VariacionModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface VariacionRepository extends JpaRepository<VariacionModel, Long> {
+
+    VariacionModel findByNombre(String nombre);
+
+    List<VariacionModel> findAllByNombreContaining(String nombre);
+
+   
+
+
+    
+}
